b540c348874d5819246b76564b591ede95c99651
FreetimeMaker <jamie.achatz@s-stb.ch>
2025-10-27 11:33:46 +0100

    changed, added and deleted

--- app/src/main/java/com/freetime/ssmpc/MainActivity.java
+++ app/src/main/java/com/freetime/ssmpc/MainActivity.java
@@ -35,5 +35,11 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // 🔄 In-App-Updater starten
+        new InAppUpdater(this)
+            .setUpdateSource(UpdateSource.GITHUB)
+            .setGitHubUserAndRepo("FreetimeMaker", "SuperSMP-Companion-App")
+            .start();
     }
 }
