8633e846ce178513b3e048a45453f97fa5948e2d
Freetime Maker <jamieachatz@gmail.com>
2026-01-30 18:17:06 +0100

    Changed the Gradle Files

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
+    id("com.google.devtools.ksp") version "2.3.4" apply false
 }
