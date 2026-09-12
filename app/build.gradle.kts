a4c3cab85bdb19736a1e44ebc9268c38eb3b08e4
Jamie Achatz <jamieachatz@gmail.com>
2026-09-12 16:53:49 +0200

    Update version to 1.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.freetime.ssmpc"
         minSdk = 26
         targetSdk = 37
-        versionCode = 20
-        versionName = "1.6.0"
+        versionCode = 21
+        versionName = "1.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
