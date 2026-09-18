2742e4eaf876ee2c6dc81dec9be8b59f6dc43db4
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-18 21:58:37 +0200

    Add Liquid Glass dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.material.icons.extended)
+    implementation("io.github.fletchmckee.liquid:liquid:1.1.1")
 
     implementation(libs.appcompat)
     implementation(libs.activity)
