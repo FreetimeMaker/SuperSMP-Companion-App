0f2007e82eb3ed8a57f6aa384489afcf89f44711
Jamie Achatz <jamieachatz@gmail.com>
2026-09-19 14:34:49 +0200

    Bump version to 2.0.0 and update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.freetime.ssmpc"
         minSdk = 26
         targetSdk = 37
-        versionCode = 21
-        versionName = "1.6.1"
+        versionCode = 22
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
