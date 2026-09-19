056537dff8d59ba39c6fdb0fdef3f3e8d12db133
Freetime Maker <165287054+FreetimeMaker@users.noreply.github.com>
2026-09-19 14:09:10 +0200

    Use centralized automatic theme

--- app/src/main/java/com/freetime/ssmpc/MainActivity.kt
+++ app/src/main/java/com/freetime/ssmpc/MainActivity.kt
@@ -8,36 +8,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.freetime.ssmpc.ui.navigation.BottomNavigationBar
 import com.freetime.ssmpc.ui.navigation.SuperSMPNavigation
 import com.freetime.ssmpc.ui.theme.SuperSMPTheme
-import kotlinx.coroutines.delay
-import java.time.LocalTime
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            var currentHour by remember { mutableIntStateOf(LocalTime.now().hour) }
-
-            LaunchedEffect(Unit) {
-                while (true) {
-                    currentHour = LocalTime.now().hour
-                    delay(60_000)
-                }
-            }
-
-            val darkTheme = currentHour < 7 || currentHour >= 19
-
-            SuperSMPTheme(darkTheme = darkTheme) {
+            SuperSMPTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
